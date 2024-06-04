64ab19764356203a50c5518c7f0f8878e1786a33
Liz GingerKiwi.Dev <liz@gingerkiwi.dev>
2024-06-04 12:57:58 -0400

    first commit. WCAG1.1 already completed

---
+++ WCAG 1.1/react.jsx
@@ -0,0 +1,9 @@
+
+
+function CatImageComponent() {
+    return (
+      <img src="/assets/boo-cat.jpg" alt="Pretty grey fluffy cat smiling with sunlight shining from behind him" />
+    );
+  }
+
+  
