e877d44b8864c771beb6ef72521a3f24919651e0
Liz GingerKiwi.Dev <liz@gingerkiwi.dev>
2024-06-12 16:51:03 -0400

    add folder and files for WCAG 1.2

---
+++ WCAG 2.1/react01.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+import YoutubeEmbed from "./YoutubeEmbed";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Kitten hiking on a leash in Rouge Park</h1>
+      <YoutubeEmbed embedId="IJ1C22wCICQ" />
+      <p>Visual description: Video of a large white cat wearing a harness and a red and white bandanna walking on a leash. He's on a gravel trail with an edge of tall grass and green plants. The video is taken looking down at him because I'm walking and filming. Kitten keeps meowing in answer to my talking to him</p>
+    </div>
+  );
+}
+
